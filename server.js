6e5f41af899b77f1234e230123e472dc5997e6b7
navneetjo <navneetjo@cybage.com>
2017-05-10 18:48:20 +0530

    modified server.js Procfile

--- server.js
+++ server.js
@@ -46,5 +46,5 @@ app.use(express.static(__dirname + '/public'));
 
 
 
-app.listen(process.env.PORT || 3000);
-console.log("Express server listening on port 3000");
+app.listen(process.env.PORT || 5000);
+console.log("Express server listening on port 5000");
