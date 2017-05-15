9d9068848ca707824de05cebcfbc47dd92b95b25
navneetjo <navneetjo@cybage.com>
2017-05-15 11:46:08 +0530

    modified app.js file

---
+++ app.js
@@ -0,0 +1,75 @@
+/*
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+*/
+
+
+/*
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', function(request, response) {
+	app.use(express.static(__dirname + '/public'))
+response.sendFile(path.join(__dirname + '/index.html'));  
+})
+
+
+
+app.set('port', (process.env.PORT || 5000))
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
+
+*/
+
+/* working fine
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.listen(process.env.PORT || 5000);
+console.log("Express server listening on port 5000");
+
+*/
+
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('app is running on port', app.get('port'));
+});
