0f03666d9d3c621383609402996d9a622d07c4c8
navneetjo <navneetjo@cybage.com>
2017-05-15 17:34:21 +0530

    adding app.js and package.json

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.get('/', function(request, response) {
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test', function(err, result) {
+    client.query('SELECT random()', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
