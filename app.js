770ca9ae432fbf5ca043b07dd8bc1c0ae75c8637
navneetjo <navneetjo@cybage.com>
2017-05-15 17:24:56 +0530

    adding app.js and package.json

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.listen(process.env.PORT || 5000);
 console.log("Express server listening on port 5000");
 
 */
-
+var pg = require('pg');
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -69,6 +69,21 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+// for db connection getting query related issue
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('app is running on port', app.get('port'));
