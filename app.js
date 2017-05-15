f57cc23ef013cf5fc8b2f9c49c0ae2fe7e5a54c5
navneetjo <navneetjo@cybage.com>
2017-05-15 17:59:37 +0530

    adding app.js and package.json

--- app.js
+++ app.js
@@ -74,7 +74,10 @@ app.get('/', function(request, response) {
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('CREATE TABLE test (
+ column_name TYPE column_constraint,
+ table_constraint table_constraint
+) ', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
