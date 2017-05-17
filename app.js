9448752456a4e23373228b139baa2494d088c0b2
navneetjo <navneetjo@cybage.com>
2017-05-17 12:24:58 +0530

    adding first commit to portfolio

--- app.js
+++ app.js
@@ -53,10 +53,20 @@ console.log("Express server listening on port 5000");
 
 */
 var pg = require('pg');
+var Pool= pg.Pool;
+
 var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+
+var config = {
+  host: 'ec2-23-23-227-188.compute-1.amazonaws.com',
+  user: 'iuhujmwpdfjvig',
+  password: '10d034d0a8194c8df0bd0d610bfc2ed5f89751573d53e405de463306953d87ef',
+  database: 'd4geimhijn4al5',
+};
+var pool = new Pool(config)
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -74,7 +84,7 @@ app.get('/', function(request, response) {
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+    pool.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
@@ -83,6 +93,25 @@ app.get('/db', function (request, response) {
     });
   });
 });
+
+
+app.get('/info', function (request, response) {
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) 
+	  { console.error(err); response.send("Error " + err); }
+  console.log('Connected to postgres! Getting schemas...');
+	console.log("Client info:",client)
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+});
+
+
+
 
 
 app.listen(app.get('port'), function() {
